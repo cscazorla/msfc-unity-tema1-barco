ebf04bb533e0462db801b1a39c8668c628308526
Carlos Sánchez Cazorla <csanchez@bitnami.com>
2017-09-03 22:25:14 +0200

    First commit

---
+++ Assets/Velocity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Velocity : MonoBehaviour {
+
+	public Vector3 vectorVelocidadBarco;
+	public Vector3 vectorVelocidadCorriente;
+	public Vector3 vectorVelocidadNeta;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Debug.DrawLine(Vector3.zero, vectorVelocidadCorriente, Color.yellow);
+		Debug.DrawLine(Vector3.zero, vectorVelocidadBarco, Color.green);
+		vectorVelocidadNeta = vectorVelocidadCorriente + vectorVelocidadBarco;
+		transform.position += vectorVelocidadNeta * Time.deltaTime;
+	}
+}
